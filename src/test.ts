9acd89f5bd203d8cd67bdf17ae1582a417240bff
rumesh <rumeshk066@gmail.com>
2025-12-08 20:57:08 +0530

    first commit

---
+++ src/test.ts
@@ -0,0 +1,10 @@
+let age: number = 22;
+let name: string = "Rumesh";
+let isStudent: boolean = true;
+
+// function example
+function greet(person: string): string {
+  return `Hi ${person}!`;
+}
+
+console.log(greet(name));
